65e845112164ff1bbfe643d63e1869c37ec79541
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-05-12 12:01:04 +0200

    storyboard fixed with CE,C,%,X,/,+,-,= buttons

---
+++ CountOnMe/SimpleCalc.swift
@@ -0,0 +1,14 @@
+//
+//  ArithmetiqueUnit.swift
+//  CountOnMe
+//
+//  Created by Laurent Debeaujon on 06/05/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class SimpleCalc {
+
+}
+
--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -11,16 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
-    
-   
-    
    
     // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
     
     /// View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
@@ -90,6 +86,23 @@ class ViewController: UIViewController {
         
         textView.text.append(" = \(operationsToReduce.first!)")
     }
-
+    
+    var elements: [String] {
+          return textView.text.split(separator: " ").map { "\($0)" }
+      }
+    // Error check computed variables
+      var expressionIsCorrect: Bool {
+          return elements.last != "+" && elements.last != "-"
+      }
+      
+      var expressionHaveEnoughElement: Bool {
+          return elements.count >= 3
+      }
+      var canAddOperator: Bool {
+             return elements.last != "+" && elements.last != "-"
+         }
+     var expressionHaveResult: Bool {
+           return textView.text.firstIndex(of: "=") != nil
+       }
 }
 
