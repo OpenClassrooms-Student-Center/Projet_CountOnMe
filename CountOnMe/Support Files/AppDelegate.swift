bb88575443c61641980bcc4bed3a3df2144a90a8
Thomas Heinis <thomas@late25.com>
2022-08-18 22:04:29 +0200

    Initial commit - project development team - storyboard - SwiftLint - Exception pointbreak - Project directories - Info.plist

---
+++ CountOnMe/Support Files/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  SimpleCalc
+//
+//  Created by Vincent Saluzzo on 29/03/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) { }
+
+  func applicationDidEnterBackground(_ application: UIApplication) { }
+
+  func applicationWillEnterForeground(_ application: UIApplication) { }
+
+  func applicationDidBecomeActive(_ application: UIApplication) { }
+
+  func applicationWillTerminate(_ application: UIApplication) { }
+}
