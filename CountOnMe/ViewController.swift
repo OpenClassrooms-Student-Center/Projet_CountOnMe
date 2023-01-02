2a3d113ea91f6a470570b7f4d52a2d11d3a91837
Cedios <ced.iosdev@icloud.com>
2023-01-02 20:00:45 +0100

    Adding Multiplication and Division Buttons in Storyboard and ViewController.

--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -72,6 +72,27 @@ class ViewController: UIViewController {
             self.present(alertVC, animated: true, completion: nil)
         }
     }
+    
+ @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
+         if canAddOperator {
+             textView.text.append(" x ")
+         } else {
+             let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+             self.present(alertVC, animated: true, completion: nil)
+         }
+     }
+     
+     @IBAction func tappedDivisionButton(_ sender: UIButton) {
+         if canAddOperator {
+             textView.text.append(" / ")
+         } else {
+             let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+             self.present(alertVC, animated: true, completion: nil)
+         }
+     }
+
 
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         guard expressionIsCorrect else {
@@ -99,6 +120,8 @@ class ViewController: UIViewController {
             switch operand {
             case "+": result = left + right
             case "-": result = left - right
+            case "x": result = left * right
+            case "/": result = left / right
             default: fatalError("Unknown operator !")
             }
             
